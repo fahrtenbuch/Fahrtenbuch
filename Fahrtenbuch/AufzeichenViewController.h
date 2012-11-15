5e154d6e7c609ff348d3ca2129473abc9f6e1774
Julian Heidenreich <heidenreich@lavabit.com>
2012-11-15 22:29:02 +0100

    just try a commit before editing db-service

--- Fahrtenbuch/AufzeichenViewController.h
+++ Fahrtenbuch/AufzeichenViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface AufzeichenViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UILabel *lbl_city;
+@property (weak, nonatomic) IBOutlet UILabel *lbl_distance;
+@property (weak, nonatomic) IBOutlet UILabel *lbl_speed;
+@property (weak, nonatomic) IBOutlet UILabel *lbl_startTime;
+@property (weak, nonatomic) IBOutlet UILabel *lbl_endTime;
+
+- (IBAction)btn_start:(id)sender;
+- (IBAction)btn_stop:(id)sender;
+
 @end
+
+
+CLLocationManager *locationManagerRoute;
+CLLocationManager *locationManagerCityName;
+
+NSDateFormatter *dateFormatter;
+NSDate *start;
+
+double distance;
--- Fahrtenbuch/AufzeichenViewController.m
+++ Fahrtenbuch/AufzeichenViewController.m
@@ -9,6 +9,96 @@
 #import "AufzeichenViewController.h"
 
 @implementation AufzeichenViewController
+@synthesize lbl_city;
+@synthesize lbl_distance;
+@synthesize lbl_speed;
+@synthesize lbl_startTime;
+@synthesize lbl_endTime;
+
+
+#pragma mark - viewDidLoad on RecordViewController
+
+/*- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"dd.MM HH:mm"];
+    
+    locationManagerCityName = [[CLLocationManager alloc] init];
+    locationManagerCityName.delegate = self;
+    locationManagerCityName.distanceFilter = kCLDistanceFilterNone;
+    locationManagerCityName.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    [locationManagerCityName startUpdatingLocation];
+    
+}*/
+
+
+#pragma mark - locationManager record methods
+
+//methode to handle current location
+-(void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    
+    if (manager==locationManagerCityName) {
+        
+        CLGeocoder *geoCoder = [[CLGeocoder alloc] init];
+        [geoCoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+            
+            for (CLPlacemark *placemark in placemarks) {
+                
+                //concatination of adress
+                /*NSString *address = [NSString stringWithFormat:@"%@ %@, %@ %@",
+                 [placemark subThoroughfare], [placemark thoroughfare], [placemark locality], [placemark administrativeArea]];
+                 */
+                //labelCity.text = address;
+                lbl_city.text = [placemark locality];
+                [locationManagerCityName stopUpdatingLocation];
+            }
+        }];
+    }
+    
+    else if (manager==locationManagerRoute) {
+        
+        //calculate average route
+        distance  += [newLocation distanceFromLocation:oldLocation];
+        
+        lbl_distance.text = [[NSString alloc] initWithFormat:@"%i", (int)distance];
+        
+        //calculate avarage speed
+        lbl_speed.text = [[NSString alloc] initWithFormat:@"%i", (int)(distance/([start timeIntervalSinceNow]*-1)*3.6)];
+    }
+}
+
+#pragma mark - Button methods to handle location record
+
+//methods to start and stop watching
+- (IBAction)btn_start:(id)sender {
+    
+    start = [NSDate date];
+    
+    lbl_startTime.text = [dateFormatter stringFromDate:start];
+    lbl_endTime.text = @"00.00 00:00";
+    
+    distance = 0;
+    locationManagerRoute = [[CLLocationManager alloc] init];
+    locationManagerRoute.delegate = self;
+    
+    //soll in App einstellbar sein
+    locationManagerRoute.distanceFilter = kCLDistanceFilterNone;
+    
+    locationManagerRoute.desiredAccuracy = kCLLocationAccuracyHundredMeters;
+    [locationManagerRoute startUpdatingLocation];
+    
+}
+
+- (IBAction)btn_stop:(id)sender {
+    
+    [locationManagerRoute stopUpdatingLocation];
+    lbl_endTime.text = [dateFormatter stringFromDate:[NSDate date]];
+}
+
+
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -46,6 +136,11 @@
 
 - (void)viewDidUnload
 {
+    [self setLbl_city:nil];
+    [self setLbl_distance:nil];
+    [self setLbl_speed:nil];
+    [self setLbl_startTime:nil];
+    [self setLbl_endTime:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -56,5 +151,6 @@
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
+
 
 @end
